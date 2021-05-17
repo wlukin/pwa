36563d74cf48bd54defbc8785f6ad9dbbbe69b3e
blade <blade@zeoalliance.com>
2021-05-17 21:05:15 +0300

    img

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 const container = document.querySelector(".container")
 const coffees = [
-    { name: "Perspiciatis", image: "images/coffee1.jpg" },
-    { name: "Voluptatem", image: "images/coffee2.jpg" },
-    { name: "Explicabo", image: "images/coffee3.jpg" },
-    { name: "Rchitecto", image: "images/coffee4.jpg" },
-    { name: " Beatae", image: "images/coffee5.jpg" },
-    { name: " Vitae", image: "images/coffee6.jpg" },
-    { name: "Inventore", image: "images/coffee7.jpg" },
-    { name: "Veritatis", image: "images/coffee8.jpg" },
-    { name: "Accusantium", image: "images/coffee9.jpg" },
+    { name: "Perspiciatis", image: "images/coffee.jpg" },
+    { name: "Voluptatem", image: "images/coffee.jpg" },
+    { name: "Explicabo", image: "images/coffee.jpg" },
+    { name: "Rchitecto", image: "images/coffee.jpg" },
+    { name: " Beatae", image: "images/coffee.jpg" },
+    { name: " Vitae", image: "images/coffee.jpg" },
+    { name: "Inventore", image: "images/coffee.jpg" },
+    { name: "Veritatis", image: "images/coffee.jpg" },
+    { name: "Accusantium", image: "images/coffee.jpg" },
 ]
 
 const showCoffees = () => {
